1c8d1343ac17d6957d11b6a6dcde871365a67e7b
Ventsislava V Genova <73789010+moontino@users.noreply.github.com>
2023-02-03 19:08:50 +0200

    Start project

--- gooddoc-react-app/src/App.js
+++ gooddoc-react-app/src/App.js
@@ -29,7 +29,7 @@ console.log(setUser)
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn Reactv
+          Start project
         </a>
       </header>
     </div>
